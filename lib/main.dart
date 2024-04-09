a3c59d631ec4a971061907067c90ca7042c44809
Mahmood <shahmahmoodhashemi132@gmail.com>
2024-04-09 15:38:08 +0430

    Initial commit
    The textfields of sign up page were created

---
+++ lib/main.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Login Page',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Sign in and Sign up'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xff15181F),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+          left: 300,
+          top: -40,
+          child: ClipOval(
+            child: Container(
+              color: Color(0xff1B1E27),
+              width: 200,
+              height: 200,
+            ),
+          ), ),
+          Positioned(
+              left: -80,
+              top: 500,
+              child: ClipOval(
+                child: Container(
+                  color: Color(0xff1B1E27),
+                  width: 200,
+                  height: 200,
+                ),
+              )),
+           Padding(
+            padding: EdgeInsets.only(left: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: 30,
+                      bottom: 7
+                  ),
+                  child: Text("Sign up",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40,
+                    color: Color(0xff6E9AE5),
+                  ),
+                  ),
+                ),
+                Text("Create an account to get Started",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white
+                ),
+
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 18,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 25),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          child: TextField(
+                            style: TextStyle(
+                              color: Color(0xff6386C9),
+                            ),
+                            cursorColor: Color(0xff6386C9),
+                            decoration: InputDecoration(
+                              fillColor: Color(0xff101115),
+                              filled: true,
+                              hintText: "Name",
+                              hintStyle: TextStyle(
+                                color: Color(0xff373945),
+                              ),
+                              border: OutlineInputBorder(
+                               borderRadius: BorderRadius.circular(40),
+                                borderSide: BorderSide.none,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40),
+                                borderSide: BorderSide(
+                                  color: Color(0xff6386C9),
+                                  width: 2,
+                                )
+                              )
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 10,
+                          ),
+                          child: TextField(
+                            style: TextStyle(
+                              color: Color(0xff6386C9),
+                            ),
+                            cursorColor: Color(0xff6386C9),
+                            decoration: InputDecoration(
+                              fillColor: Color(0xff101115),
+                              filled: true,
+                              hintText: "Email",
+                              hintStyle: TextStyle(
+                                color: Color(0xff373945),
+                              ),
+                              border: OutlineInputBorder(
+                               borderRadius: BorderRadius.circular(40),
+                                borderSide: BorderSide.none,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40),
+                                borderSide: BorderSide(
+                                  color: Color(0xff6386C9),
+                                  width: 2,
+                                )
+                              )
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 10,
+                          ),
+                          child: TextField(
+                            style: TextStyle(
+                              color: Color(0xff6386C9),
+                            ),
+                            cursorColor: Color(0xff6386C9),
+                            decoration: InputDecoration(
+                              fillColor: Color(0xff101115),
+                              filled: true,
+                              hintText: "Password",
+                              hintStyle: TextStyle(
+                                color: Color(0xff373945),
+                              ),
+                              border: OutlineInputBorder(
+                               borderRadius: BorderRadius.circular(40),
+                                borderSide: BorderSide.none,
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40),
+                                borderSide: BorderSide(
+                                  color: Color(0xff6386C9),
+                                  width: 2,
+                                )
+                              )
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
